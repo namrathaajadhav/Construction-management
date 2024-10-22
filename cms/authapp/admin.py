9a05c758fdd64be7993bfdfbbae9d982fe64dd41
Namratha <namrathanjjadhav@gmail.com>
2024-10-22 13:08:28 +0530

    ANK GROUP

--- cms/authapp/admin.py
+++ cms/authapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+ 
+# Register your models here.
+from django.contrib import admin
+from .models import User, Manager, Supervisor, Project, Task
+ 
+admin.site.register(User)
+admin.site.register(Manager)
+admin.site.register(Supervisor)
+admin.site.register(Project)
+admin.site.register(Task)
+ 
--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
+ 
+AUTH_USER_MODEL = 'authapp.User'
